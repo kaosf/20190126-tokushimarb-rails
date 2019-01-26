a26823fe223e21479f2173a9db1e827e6aebabb5
ka <ka.kaosf@gmail.com>
2019-01-26 13:50:42 +0900

    Implement MFA feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  resource :two_factor_auth, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
